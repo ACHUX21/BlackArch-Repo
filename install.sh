169114b6ad725b01a3c393be9cd5d069704235f7
ACHUX21 <130113878+ACHUX21@users.noreply.github.com>
2023-12-12 14:38:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ cd Tools-BlackArch/
 sudo chmod +x strap.sh
 sudo ./strap.sh
 
+echo "BlackArch repo added Successfully"
 
-for pkg in $(sudo pacman -Sgg | awk '/blackarch/ {print $2}' | sort -u); do
-    sudo pacman -S --noconfirm $pkg
-done
+echo -n "You want to Install All the $(sudo pacman -Sgg | awk '/blackarch/ {print $2}' | sort -u) tools (N/Y) : "
+read des
+
+if [ -z "$des" ] || [ "$des" == "Y" ] || [ "$des" == "y" ]; then
+
+    for pkg in $(sudo pacman -Sgg | awk '/blackarch/ {print $2}' | sort -u); do
+        sudo pacman -S --noconfirm $pkg
+    done
+fi
